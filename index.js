571c1cb803b93de3f00d7a77d4b359f3a3372f96
Cam <farrcameron@gmail.com>
2023-10-11 20:19:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const express = require('express')
 
 require('dotenv').config()
 const PORT = process.env.PORT
+const app = express()
 console.log(PORT);
 
-app.get('/', (req,res) => {
-    res.send('Welcome to an Awesome App about Breads!');
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads!')
 })
 
 const breadsController = require('./controllers/breads_controller.js')
